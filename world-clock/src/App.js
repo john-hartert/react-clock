20df30703b030fa7dfe3a77a30cdb3f29a6e21fc
John Hartert <j.blainehartert@gmail.com>
2017-08-30 13:30:34 -0400

    Adds timedisplay

--- world-clock/src/App.js
+++ world-clock/src/App.js
@@ -4,13 +4,15 @@ import logo from './logo.svg';
 import './App.css';
 
 
-import PlaceName from './PlaceName';
+import PlaceName from './PlaceName.js';
+import TimeDisplay from './TimeDisplay.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <PlaceName name="Atlanta" />
+        <TimeDisplay time={new Date()} />
       </div>
     );
   }
---
+++ world-clock/src/TimeDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TimeDisplay = ({time}) => (
+    <p>
+        {time}
+    </p>
+
+);
+
+export default TimeDisplay;
